d6481f54f5ce3a5c716a7b9ab88fc8556b582297
Jacob Ford <ford.jacob.ford@gmail.com>
2016-12-10 01:39:05 +0000

    Added urls and index view

--- Project/Edulution/views.py
+++ Project/Edulution/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponseRedirect,\
+    HttpResponse, HttpRequest, Http404
 
-# Create your views here.
+def index(request):
+    """
+
+    :param request: HttpRequest
+    :return: HttpResponse
+    """
+
+    return render(request, "Edulution/index.html")
